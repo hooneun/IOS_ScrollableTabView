294723708fa6f2fbabab6756353aaabf209a6a65
hooneun <kimhoon0316@gmail.com>
2024-04-23 20:08:47 +0900

    init

---
+++ ScrollableTabView/ScrollableTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabViewApp.swift
+//  ScrollableTabView
+//
+//  Created by Hoon on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
